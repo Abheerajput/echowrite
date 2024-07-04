21b3cafcf683b365d1b53afeb1ba05781b92caf9
Abheerajput <ar0776631@gmail.com>
2024-07-04 22:56:22 +0530

    scrolling

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -10,18 +10,26 @@ import Brand7 from "../assets/svg/Brand7.svg";
 const Brands = () => {
   return (
     <div className="text-center px-3 pt-12">
-      <h1 className="font-bold text-3xl md:text-4xl text-[#161C2D]">
+      <h1 className="font-bold text-3xl inter_ff md:text-4xl text-[#161C2D]">
         Trusted by Various Brands
       </h1>
       <div className="overflow-hidden w-full h-24 md:h-28 lg:h-32 mt-4">
-        <div className="marquee flex space-x-4">
-          <img src={Brand1} alt="Brand1" className="h-full" />
-          <img src={Brand2} alt="Brand2" className="h-full" />
-          <img src={Brand3} alt="Brand3" className="h-full" />
-          <img src={Brand4} alt="Brand4" className="h-full" />
-          <img src={Brand5} alt="Brand5" className="h-full" />
-          <img src={Brand6} alt="Brand6" className="h-full" />
-          <img src={Brand7} alt="Brand7" className="h-full" />
+        <div className="marquee flex">
+          <img src={Brand1} alt="Brand1" className="h-full mx-4" />
+          <img src={Brand2} alt="Brand2" className="h-full mx-4" />
+          <img src={Brand3} alt="Brand3" className="h-full mx-4" />
+          <img src={Brand4} alt="Brand4" className="h-full mx-4" />
+          <img src={Brand5} alt="Brand5" className="h-full mx-4" />
+          <img src={Brand6} alt="Brand6" className="h-full mx-4" />
+          <img src={Brand7} alt="Brand7" className="h-full mx-4" />
+          {/* Duplicate images for smooth scrolling effect */}
+          <img src={Brand1} alt="Brand1" className="h-full mx-4" />
+          <img src={Brand2} alt="Brand2" className="h-full mx-4" />
+          <img src={Brand3} alt="Brand3" className="h-full mx-4" />
+          <img src={Brand4} alt="Brand4" className="h-full mx-4" />
+          <img src={Brand5} alt="Brand5" className="h-full mx-4" />
+          <img src={Brand6} alt="Brand6" className="h-full mx-4" />
+          <img src={Brand7} alt="Brand7" className="h-full mx-4" />
         </div>
       </div>
     </div>
