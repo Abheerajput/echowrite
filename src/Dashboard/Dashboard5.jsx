0ca81f0a89cdce184c9e5ad2cc91e18dc7f7d3b4
Abheerajput <ar0776631@gmail.com>
2024-07-05 13:16:58 +0530

    dashboard

--- src/Dashboard/Dashboard5.jsx
+++ src/Dashboard/Dashboard5.jsx
@@ -88,7 +88,9 @@ const Dashboard5 = () => {
            <div className='flex flex-col w-full    xs:flex xs:flex-col xs:justify-aroundl'>
 <div className='flex pt-4 xs:flex xs:flex-col xs:justify-around'>
 <div className='flex flex-col items-center w-2/5 xs:flex xs:flex-col xs:py-6 xs:gap-3 xs:justify-around xs:w-full xs:mb-2'>
-
+<p className='flex justify-center'>
+              <img className='w-24 h-24' src={mike} alt="Microphone" />
+            </p>
                   <p className='text-[18px] font-bold poppins'>00:55 mins</p>
                   <p className='text-[#808080] text-nowrap text-[15px] font-normal'>Support Multiple Language.</p>
             <p className='pt-2'>
