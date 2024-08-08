0ec8371581867eacf9ae8badf585a1d7a6a4618d
Abheerajput <ar0776631@gmail.com>
2024-08-08 21:51:07 +0530

    Webview

--- src/Dashboard/webView.jsx
+++ src/Dashboard/webView.jsx
@@ -441,13 +441,13 @@ const Dashboard2 = () => {
                   <div className='flex flex-col gap-3 md:hidden lg:hidden'>
                   <button
     onClick={handleUploadDocument}
-    className="bg-green-500 text-white px-4 py-2 rounded mt-4">
+    className="bg-blue-500 text-white px-4 py-2 rounded-xl mt-4">
     Upload Document
   </button>
 
 <button
     onClick={handleDownload}
-    className="bg-green-500 text-white px-4 py-2 rounded mt-4"
+    className="bg-blue-500 text-white px-4 py-2 rounded-xl mt-4"
   >
     Download Document
   </button>
